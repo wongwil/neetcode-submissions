21b41d04ec41c394535e15b2a8ccb2a60494d4c2
William Wong <williamwong1333@hotmail.com>
2026-08-24 20:10:50 +0200

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        subres = []
+
+        def dfs(i):
+            if i == len(s): # only add when we reached the end -> it means we were able to split
+            # the whole s into palindrome substrings
+                res.append(subres.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    subres.append(s[i:j+1])
+                    
+                    dfs(j+1)
+                    subres.pop()
+
+        dfs(0)
+
+        return res
+            
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
